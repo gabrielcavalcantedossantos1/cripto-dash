e18e443265c72e68339425db0009b366181b3217
Gabriel Cavalcante <gabrielcavalcantedossantos1@gmail.com>
2026-03-11 13:11:40 -0300

    Enviando o provider do context

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,17 @@ import { createRoot } from "react-dom/client";
 
 import { BrowserRouter } from "react-router";
 
+import { ContextProvider } from "./context/context.jsx";
+
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ContextProvider>
   </StrictMode>,
 );
