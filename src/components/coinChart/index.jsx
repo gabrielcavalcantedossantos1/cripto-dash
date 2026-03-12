9fa50e76a3e58a6b1b6a58480934cc0ae0fd3952
Gabriel Cavalcante <gabrielcavalcantedossantos1@gmail.com>
2026-03-12 18:16:04 -0300

    grafico finalizado

--- src/components/coinChart/index.jsx
+++ src/components/coinChart/index.jsx
@@ -45,12 +45,14 @@ export function CoinChart({ coinId }) {
         }
 
         const data = await response.json();
+
         const prices = data.prices.map((price) => ({
           x: price[0],
           y: price[1],
         }));
 
         console.log("Preços", prices);
+
         setChartData({
           datasets: [
             {
@@ -65,7 +67,7 @@ export function CoinChart({ coinId }) {
           ],
         });
       } catch (error) {
-        setError("Ocorreu um erro ao buscar as moedas");
+        setError("Ocorreu um erro ao carregar o gráfico");
         console.error("Error:", error);
       } finally {
         setLoading(false);
@@ -75,5 +77,64 @@ export function CoinChart({ coinId }) {
     fetchPrices();
   }, [coinId, days]);
 
-  return <>chart</>;
+  if (loading) return <div className="spinner"></div>;
+  if (error) return <p>{error}</p>;
+  if (!chartData) return null;
+
+  const options = [
+    { label: "1 dia", value: 1 },
+    { label: "3 dias", value: 3 },
+    { label: "7 dias", value: 7 },
+    { label: "15 dias", value: 15 },
+    { label: "30 dias", value: 30 },
+    { label: "60 dias", value: 60 },
+    { label: "90 dias", value: 90 },
+    { label: "180 dias", value: 180 },
+    { label: "365 dias", value: 365 },
+  ];
+
+  return (
+    <div style={{ marginTop: "30px", width: "100%", height: "400px" }}>
+      <label htmlFor="dias">Mudança de dias para o grafico</label>{" "}
+      <select id="dias" onChange={(e) => setDays(Number(e.target.value))} className="days-select">
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+
+      <Line
+        data={chartData}
+        options={{
+          responsive: true,
+          plugins: {
+            legend: { display: false },
+            tooltip: { mode: "index", intersect: false },
+          },
+          scales: {
+            x: {
+              type: "time",
+              time: {
+                unit: "day",
+              },
+              ticks: {
+                autoSkip: true,
+                maxTicksLimit: 365,
+              },
+            },
+            y: {
+              ticks: {
+                callback: (v) =>
+                  v.toLocaleString("pt-BR", {
+                    style: "currency",
+                    currency: "BRL",
+                  }),
+              },
+            },
+          },
+        }}
+      />
+    </div>
+  );
 }
